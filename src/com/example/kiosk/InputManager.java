68af7ba11de15156ee1119bf81903bfc613bf89c
dodu232 <dodu7510@gmail.com>
2025-03-07 16:59:54 +0900

    refactor: InputManager
    checkRange 메소드 추가

--- src/com/example/kiosk/InputManager.java
+++ src/com/example/kiosk/InputManager.java
@@ -1,5 +1,6 @@
 package com.example.kiosk;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputManager {
@@ -13,9 +14,24 @@ public class InputManager {
         while(true){
             try{
                 return sc.nextInt();
-            }catch(Exception e){
-                e.printStackTrace();
+            }catch(InputMismatchException e){
+                System.out.println("정수를 입력하세요.");
             }
         }
     }
+
+    public int checkRange(int max){
+        while(true){
+            try {
+                int select = inputInt();
+                if(select < 0 || select > max){
+                    throw new OutOfRangeException("메뉴 안에 있는 값만 입력하세요.");
+                }
+                return select;
+            } catch (OutOfRangeException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 }
