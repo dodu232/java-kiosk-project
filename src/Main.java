1c917c30c4238af44ce49612a205fc1f7b549def
dodu232 <dodu7510@gmail.com>
2025-03-10 14:56:57 +0900

    feat: lv3 코드 작성

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import com.example.kiosk.Kiosk;
+
 public class Main {
     public static void main(String[] args) {
+        Kiosk kiosk = new Kiosk();
 
+        kiosk.start();
     }
 }
--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -5,28 +5,28 @@ import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        // List 선언 및 초기화
-        List<MenuItem> list = new ArrayList<>();
-        final int END = -1;
+public class Kiosk {
+    private final List<MenuItem> menuItemList = new ArrayList<>();
+    private final Scanner sc;
 
+    public Kiosk(){
+        sc = new Scanner(System.in);
+        menuItemList.add(new MenuItem("클래식 버거", 4900, "토마토, 양상추, 케찹이 들어간 기본 버거"));
+        menuItemList.add(new MenuItem("치즈 버거  ", 5400, "치즈와 케찹이 들어간 혈관 막히는 치즈 버거"));
+        menuItemList.add(new MenuItem("불고기 버거", 5900, "불고기 소스가 들어간 다 아는 맛, 불고기 버거"));
+        menuItemList.add(new MenuItem("새우 버거  ", 5400, "새우가 들어간 새우 버거"));
+    }
+
+    public void start(){
         System.out.println("----------- 도두 버거 -----------");
         System.out.println("도두 버거에 오신것을 환영합니다.");
 
-        // add 함수를 통해 new MenuItem(이름, 가격, 설명) List에 삽입
-        list.add(new MenuItem("클래식 버거", 4900, "토마토, 양상추, 케찹이 들어간 기본 버거"));
-        list.add(new MenuItem("치즈 버거  ", 5400, "치즈와 케찹이 들어간 혈관 막히는 치즈 버거"));
-        list.add(new MenuItem("불고기 버거", 5900, "불고기 소스가 들어간 다 아는 맛, 불고기 버거"));
-        list.add(new MenuItem("새우 버거  ", 5400, "새우가 들어간 새우 버거"));
-
-        // Scanner 선언
-        Scanner sc = new Scanner(System.in);
+        final int END = -1;
 
         while (true) {
             // 반복문을 활용해 List 안에 있는 MenuItem을 하나씩 출력
-            for (int i = 0; i < list.size(); i++) {
-                System.out.println((i + 1) + list.get(i).toString());
+            for (int i = 0; i < menuItemList.size(); i++) {
+                System.out.println((i + 1) + menuItemList.get(i).toString());
             }
             System.out.println("0 : 종료");
 
@@ -36,12 +36,15 @@ public class Main {
                 System.out.print("메뉴를 선택해 주세요: ");
                 try {
                     select = sc.nextInt() - 1;
-                    if (select < list.size() && select >= END) {
+                    sc.nextLine();
+
+                    if (select < menuItemList.size() && select >= END) {
                         break;
                     }
                     System.out.println("메뉴 판에 있는 번호를 입력하세요. ");
                 } catch (InputMismatchException e) {
                     System.out.println("정수를 입력하세요.");
+                    sc.nextLine();
                 }
             }
 
@@ -51,7 +54,7 @@ public class Main {
             }
 
             // 선택한 메뉴 : 이름, 가격, 설명
-            System.out.println(list.get(select).getName() + "는 " + list.get(select).getPrice() + "원 입니다.");
+            System.out.println(menuItemList.get(select).getName() + "는 " + menuItemList.get(select).getPrice() + "원 입니다.");
             System.out.println();
         }
 
