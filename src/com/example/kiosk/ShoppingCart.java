205dca2c219e0acd9c4f32d5116b226ae6da2778
dodu232 <dodu7510@gmail.com>
2025-03-11 22:13:19 +0900

    refactor: ShoppingCart 클래스 수정
    장바구니 안의 전체 상품 값 변수 total 추가
    그에 따른 생성자와 addShoppingCart 수정
    물건을 구매할 때만 계산을 하도록 수정

--- src/com/example/kiosk/ShoppingCart.java
+++ src/com/example/kiosk/ShoppingCart.java
@@ -4,28 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-    private final List<MenuItem> shoppingList;
 
-    //  생성자
-    public ShoppingCart(){
-        shoppingList = new ArrayList<>();
-    }
+  private final List<MenuItem> shoppingList;
+  private int total;
 
-    // 장바구니 추가
-    public void addShoppingCart(MenuItem menu){
-        shoppingList.add(menu);
-    }
+  //  생성자
+  public ShoppingCart() {
+    this.shoppingList = new ArrayList<>();
+    this.total = 0;
+  }
 
-    // 장바구니 초기화
-    public void resetShoppingCart(){
-        shoppingList.clear();
-    }
+  // 장바구니 추가
+  public void addShoppingCart(MenuItem menu) {
+    this.shoppingList.add(menu);
+    this.total += menu.getPrice();
+  }
 
-    // 장바구니 목록 출력
-    public void printShoppingCart(){
-        for(MenuItem item: shoppingList){
-            System.out.println(item.toString());
-        }
-        System.out.println();
+  // 장바구니 초기화
+  public void resetShoppingCart() {
+    this.shoppingList.clear();
+  }
+
+  // 장바구니 목록 출력
+  public void printShoppingCart() {
+    int sum = 0;
+    for (MenuItem item : this.shoppingList) {
+      System.out.println(item.toString());
+      sum += item.getPrice();
     }
+    System.out.println(sum);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("장바구니 상품: %s개, 합계: %d원", this.shoppingList.size(), this.total);
+  }
 }
