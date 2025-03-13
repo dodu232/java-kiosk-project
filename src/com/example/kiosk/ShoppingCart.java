4a67daba2d9ff1d916920ead9c23649949053016
dodu232 <dodu7510@gmail.com>
2025-03-13 16:02:38 +0900

    refactor: ShoppingCart 클래스 수정
    List에서 Map으로 수정
    관련 메소드 수정

--- src/com/example/kiosk/ShoppingCart.java
+++ src/com/example/kiosk/ShoppingCart.java
@@ -1,23 +1,23 @@
 package com.example.kiosk;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 // 메뉴 아이템을 장바구니에 추가하고 총 가격을 관리
 public class ShoppingCart {
 
-  private final List<MenuItem> shoppingList;
+  private final Map<MenuItem, Integer> shoppingList;
   private int totalPrice;
 
   //  생성자
   public ShoppingCart() {
-    this.shoppingList = new ArrayList<>();
+    this.shoppingList = new HashMap<>();
     this.totalPrice = 0;
   }
 
   // 장바구니 추가
   public void addMenuItem(MenuItem menu) {
-    this.shoppingList.add(menu);
+    this.shoppingList.put(menu, this.shoppingList.getOrDefault(menu, 0) + 1);
     this.totalPrice += menu.getPrice();
   }
 
@@ -40,9 +40,9 @@ public class ShoppingCart {
       return;
     }
     System.out.println("----장바구니 목록----");
-    for (int i = 0; i < this.shoppingList.size(); i++) {
-      System.out.println((i + 1) + this.shoppingList.get(i).toString());
-    }
+    this.shoppingList.forEach((key, value) -> {
+      System.out.println(key.toShoppingCartList() + value + "개  |  ￦ " + (key.getPrice() * value));
+    });
     System.out.println(this);
   }
 
