576a1403b7006aed3f2822ebe898aca8937cbc0f
dodu232 <dodu7510@gmail.com>
2025-03-13 15:00:44 +0900

    refactor: ShoppingCart 클래스 수정
    클래스 설명 추가
    필드 이름 구체적으로 변경
    메소드 이름 기능에 맞게 변경
    printShoppingCart 메소드 수정

--- src/com/example/kiosk/ShoppingCart.java
+++ src/com/example/kiosk/ShoppingCart.java
@@ -3,40 +3,44 @@ package com.example.kiosk;
 import java.util.ArrayList;
 import java.util.List;
 
+// 메뉴 아이템을 장바구니에 추가하고 총 가격을 관리
 public class ShoppingCart {
 
   private final List<MenuItem> shoppingList;
-  private int total;
+  private int totalPrice;
 
   //  생성자
   public ShoppingCart() {
     this.shoppingList = new ArrayList<>();
-    this.total = 0;
+    this.totalPrice = 0;
   }
 
   // 장바구니 추가
-  public void addShoppingCart(MenuItem menu) {
+  public void addMenuItem(MenuItem menu) {
     this.shoppingList.add(menu);
-    this.total += menu.getPrice();
+    this.totalPrice += menu.getPrice();
   }
 
   // 장바구니 초기화
   public void resetShoppingCart() {
     this.shoppingList.clear();
+    this.totalPrice = 0;
   }
 
   // 장바구니 목록 출력
   public void printShoppingCart() {
-    int sum = 0;
-    for (MenuItem item : this.shoppingList) {
-      System.out.println(item.toString());
-      sum += item.getPrice();
+    if (this.shoppingList.isEmpty()) {
+      System.out.println("장바구니가 비어 있습니다.");
+      return;
     }
-    System.out.println(sum);
+    for (int i = 0; i < this.shoppingList.size(); i++) {
+      System.out.println((i + 1) + this.shoppingList.get(i).toString());
+    }
+    System.out.println(this);
   }
 
   @Override
   public String toString() {
-    return String.format("장바구니 상품: %s개, 합계: %d원", this.shoppingList.size(), this.total);
+    return String.format("장바구니 상품: %s개, 합계: %d원", this.shoppingList.size(), this.totalPrice);
   }
 }
