b2a147bbc3e8976eec04cde6c8fe1a8a05d80ebc
dodu232 <dodu7510@gmail.com>
2025-03-13 15:03:51 +0900

    refactor: MenuItem 클래스 수정
    클래스 설명 추가
    필드에 this를 붙여서 명확하게 표시

--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -1,22 +1,24 @@
 package com.example.kiosk;
 
+// 메뉴 항목을 나타내는 클래스
 public class MenuItem {
-    private final String name;
-    private final int price;
-    private final String desc;
 
-    public MenuItem(String name, int price, String desc){
-        this.name = name;
-        this.price = price;
-        this.desc = desc;
-    }
+  private final String name;
+  private final int price;
+  private final String desc;
 
-    public int getPrice(){
-        return price;
-    }
+  public MenuItem(String name, int price, String desc) {
+    this.name = name;
+    this.price = price;
+    this.desc = desc;
+  }
 
-    @Override
-    public String toString(){
-        return String.format(" : %-5s  | ￦ %-10d | %s", name, price, desc);
-    }
+  public int getPrice() {
+    return this.price;
+  }
+
+  @Override
+  public String toString() {
+    return String.format(".  %-5s  | ￦ %-10d | %s", this.name, this.price, this.desc);
+  }
 }
